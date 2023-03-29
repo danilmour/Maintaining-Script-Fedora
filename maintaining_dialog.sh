1100e5dad18b75f5c523c900f7196a1be62eb0a9
danilmour <daniandre28@gmail.com>
2023-03-29 15:51:47 +0100

    Changed Whiptail to Dialog

--- maintaining_dialog.sh
+++ maintaining_dialog.sh
@@ -8,7 +8,7 @@
 #-------------+--------------------------------------------#
 
 # Debug Mode
-# set -x
+set -x
 
 DISTRO="Fedora"
 
@@ -21,7 +21,7 @@ function UPDATE() {
     for ((i = 0; i < 100; i += 50)); do
         echo "${i}"
         sleep 1
-    done | whiptail --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações" 10 70 0
+    done | dialog --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações" 0 0 0
 
     sudo dnf update
 
@@ -29,7 +29,7 @@ function UPDATE() {
         for ((i = 0; i < 100; i += 50)); do
             echo "${i}"
             sleep 1
-        done | whiptail --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações de pacotes Flatpak" 10 70 0
+        done | dialog --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações de pacotes Flatpak" 0 0 0
 
         sudo flatpak update
     fi
@@ -38,12 +38,12 @@ function UPDATE() {
         for ((i = 0; i < 100; i += 50)); do
             echo "${i}"
             sleep 1
-        done | whiptail --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações de pacotes Snap" 10 70 0
+        done | dialog --title "Atualização do Sistema" --gauge "O $DISTRO vai procurar agora por atualizações de pacotes Snap" 0 0 0
 
         sudo snap refresh
     fi
 
-    whiptail --title "Atualização do Sistema" --msgbox "O $DISTRO terminou as atualizações" 10 70
+    dialog --title "Atualização do Sistema" --msgbox "O $DISTRO terminou as atualizações" 0 0
 }
 
 #----------+-------------------------#
@@ -55,7 +55,7 @@ function CLEAN() {
     for ((i = 0; i < 100; i += 95)); do
         echo "${i}"
         sleep 1
-    done | whiptail --title "Limpeza do Sistema" --gauge "A começar a limpeza" 10 70 0
+    done | dialog --title "Limpeza do Sistema" --gauge "A começar a limpeza" 0 0 0
 
     sudo dnf autoremove
     sudo dnf clean all
@@ -64,7 +64,7 @@ function CLEAN() {
         sudo flatpak uninstall --unused
     fi
 
-    whiptail --title "Limpeza do Sistema" --msgbox "Sistema Limpo" 10 70
+    dialog --title "Limpeza do Sistema" --msgbox "Sistema Limpo" 0 0
 }
 
 #----------+---------------------------------------------------#
@@ -73,19 +73,18 @@ function CLEAN() {
 # Purpose  | Install a set of applications that the user wants #
 #----------+---------------------------------------------------#
 function INSTALLAPPS() {
-    PACKAGE_MANAGER=$(whiptail --title "Escolha de Gestor de Pacotes" --menu "Qual dos seguintes gestores pretende utilizar?" 10 70 0 \
+    PACKAGE_MANAGER=$(dialog --title "Escolha de Gestor de Pacotes" --menu "Qual dos seguintes gestores pretende utilizar?" 0 0 0 \
         "DNF" "" \
         "Flatpak" "" \
         "Snap" "" \
-        3>&1 1>&2 2>&3)
+        --stdout)
 
-    APPS=$(whiptail --title "Aplicações a Instalar" --inputbox "Quais aplicações quer instalar?" 10 70 \
-        3>&1 1>&2 2>&3)
+    APPS=$(dialog --title "Aplicações a Instalar" --inputbox "Quais aplicações quer instalar?" 0 0 --stdout)
 
     if [[ -z "${APPS}" ]]; then
-        whiptail --title "Aplicações a Instalar" --msgbox "Sem apps para instalar" 10 70
+        dialog --title "Aplicações a Instalar" --msgbox "Sem apps para instalar" 0 0
     else
-        whiptail --title "Instalação" --msgbox "A instalar ${APPS} com o ${PACKAGE_MANAGER}" 10 70
+        dialog --title "Instalação" --msgbox "A instalar ${APPS} com o ${PACKAGE_MANAGER}" 0 0
 
         case "${PACKAGE_MANAGER}" in
         "DNF")
@@ -96,7 +95,7 @@ function INSTALLAPPS() {
             if [ "$(command -v flatpak)" ]; then
                 sudo flatpak install "$APPS"
             else
-                whiptail --title "Erro" --msgbox "O ${PACKAGE_MANAGER} não está instalado no sistema!" 10 70
+                dialog --title "Erro" --msgbox "O ${PACKAGE_MANAGER} não está instalado no sistema!" 0 0
             fi
             ;;
 
@@ -104,12 +103,12 @@ function INSTALLAPPS() {
             if [ "$(command -v flatpak)" ]; then
                 sudo snap install "$APPS"
             else
-                whiptail --title "Erro" --msgbox "O ${PACKAGE_MANAGER} não está instalado no sistema!" 10 70
+                dialog --title "Erro" --msgbox "O ${PACKAGE_MANAGER} não está instalado no sistema!" 0 0
             fi
             ;;
         esac
 
-        whiptail --title "Aplicações a Instalar" --msgbox "Aplicações Instaladas" 10 70
+        dialog --title "Aplicações a Instalar" --msgbox "Aplicações Instaladas" 0 0
     fi
 }
 
@@ -119,7 +118,7 @@ function INSTALLAPPS() {
 # Purpose  | Show system information #
 #----------+-------------------------#
 function INFO() {
-    cat /etc/os-release
+    dialog --msgbox "$(/etc/os-release)"
 }
 
 #----------+-------------------#
@@ -128,11 +127,11 @@ function INFO() {
 # Purpose  | Turn computer off #
 #----------+-------------------#
 function TURNOFF() {
-    if (whiptail --title "Desligar o Sistema" --yesno "Tem a certeza que pretende Desligar o Sistema?" 10 70); then
+    if (dialog --title "Desligar o Sistema" --yesno "Tem a certeza que pretende Desligar o Sistema?" 10 70); then
         for ((i = 0; i < 100; i += 97)); do
             echo "${i}"
             sleep 2
-        done | whiptail --title "Desligar o Sistema" --gauge "O Sistema vai Desligar" 10 70 0
+        done | dialog --title "Desligar o Sistema" --gauge "O Sistema vai Desligar" 10 70 0
 
         sudo poweroff
     fi
@@ -144,13 +143,13 @@ function TURNOFF() {
 # Purpose  | Main menu for program #
 #----------+-----------------------#
 function MENU() {
-    CHOICE=$(whiptail --title "Menu Principal" --menu "Escolha uma opção" 20 70 0 \
+    CHOICE=$(dialog --title "Menu Principal" --menu "Escolha uma opção:" 0 0 0 \
         "Atualizar o Sistema" "" \
         "Limpar o Sistema" "" \
         "Instalar Aplicações" "" \
         "Informações do Sistema" "" \
         "Desligar o Sistema" "" \
-        3>&1 1>&2 2>&3)
+        --stdout)
 
     case "${CHOICE}" in
     "Atualizar o Sistema")
